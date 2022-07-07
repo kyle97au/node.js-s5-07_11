c7b08b4350421312dbdb0b41730174f6792c168e
kyle97au <91361399+kyle97au@users.noreply.github.com>
2022-07-07 19:47:48 +0800

    Add files via upload

---
+++ 07_11-creating-the-html-files/routes/admin.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const rootDir = require('../util/path');
+
+const express = require('express');
+const router = express.Router();
+
+// /admin/add-product => GET
+router.get('/add-product', (req, res, next) => {
+  // res.sendFile(path.join(__dirname, '..', 'views', 'add-product.html'));
+  // res.sendFile(path.join(__dirname, '../views/add-product.html'));
+  res.sendFile(path.join(rootDir, 'views/add-product.html'));
+
+  // res.send(
+  //   '<form action="/admin/hiddenadd" method="POST"><input type="text" name="titleyo"><button type="submit">Add Product</button></form>'
+  // );
+});
+
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
+});
+
+module.exports = router;
